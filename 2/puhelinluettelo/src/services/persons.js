f9c76dc718957cd8180f42f69ecfc2f0644d8602
Jakomu <janne.komul@gmail.com>
2022-08-11 15:27:59 +0300

    added error messages

--- 2/puhelinluettelo/src/services/persons.js
+++ 2/puhelinluettelo/src/services/persons.js
@@ -3,19 +3,29 @@ import axios from "axios";
 const baseUrl = "http://localhost:3001/persons";
 
 const getAll = () => {
-  return axios.get(baseUrl).then((response) => response.data);
+  return axios
+    .get(baseUrl)
+    .then((response) => response.data)
+    .catch((error) => console.log(error.message));
 };
 
 const addPerson = (newPerson) => {
-  return axios.post(baseUrl, newPerson).then((response) => response.data);
+  return axios
+    .post(baseUrl, newPerson)
+    .then((response) => response.data)
+    .catch((error) => console.log(error.message));
 };
 
 const removePerson = (id) => {
-  return axios.delete(`${baseUrl}/${id}`);
+  return axios
+    .delete(`${baseUrl}/${id}`)
+    .catch((error) => console.log(error.message));
 };
 
 const editPerson = (id, newPerson) => {
-  return axios.put(`${baseUrl}/${id}`, newPerson);
+  return axios
+    .put(`${baseUrl}/${id}`, newPerson)
+    .catch((error) => console.log(error.message));
 };
 
 export default { getAll, addPerson, removePerson, editPerson };
